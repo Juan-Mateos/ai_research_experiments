35ee9c6458d7d8c1c274c3aa818a0b15289f6b31
Juan Mateos-Garcia <juan.mateos-garcia@nesta.org.uk>
2022-07-08 08:15:43 +0100

    various install things

---
+++ ai_research_experiments/__init__.py
@@ -0,0 +1,8 @@
+"""ahl_food_reformulation."""
+import logging
+import logging.config
+from pathlib import Path
+
+
+# Define project base directory
+PROJECT_DIR = Path(__file__).resolve().parents[1]
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name="ai_research_experiments",
+    version="0.1.0",
+    author="Juan Mateos-Garcia",
+)
